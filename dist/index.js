f6231d9c08fbe34a80d6679ec92291add283b9e8
Stuart Harper <sharper@overstock.com>
2018-12-07 11:15:44 -0700

    done... out of time

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _day = require('./answers/day4');
+var _day = require('./answers/day5');
 
 // solution1_1();
 // solution1_2();
@@ -8,11 +8,14 @@ var _day = require('./answers/day4');
 // solution2_2();
 // solution3_1();
 // solution3_2();
-(0, _day.solution4_1)(); // import { solution1_1 } from './answers/day1-1';
+// solution4_1();
+// solution4_2();
+(0, _day.solution5_1)();
+// solution5_2();
+// import { solution1_1 } from './answers/day1-1';
 // import { solution1_2 } from './answers/day1-2';
 // import { solution2_1 } from './answers/day2-1';
 // import { solution2_2 } from './answers/day2-2';
 // import { solution3_1 } from './answers/day3-1';
 // import { solution3_2 } from './answers/day3-2';
-
-(0, _day.solution4_2)();
+// import { solution4_1, solution4_2 } from './answers/day4';
--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 // import { solution2_2 } from './answers/day2-2';
 // import { solution3_1 } from './answers/day3-1';
 // import { solution3_2 } from './answers/day3-2';
-import { solution4_1, solution4_2 } from './answers/day4';
+// import { solution4_1, solution4_2 } from './answers/day4';
+import { solution5_1 } from './answers/day5';
 
 
 // solution1_1();
@@ -13,5 +14,7 @@ import { solution4_1, solution4_2 } from './answers/day4';
 // solution2_2();
 // solution3_1();
 // solution3_2();
-solution4_1();
-solution4_2();
+// solution4_1();
+// solution4_2();
+solution5_1();
+// solution5_2();
